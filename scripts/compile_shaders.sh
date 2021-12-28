24e0bb8d17a4cc74531c48b4ff75cca706def33b
Tyler Mayoff <tyler@tylermayoff.com>
2021-12-28 17:57:06 -0500

    Started Graphics Pipeline setup

---
+++ scripts/compile_shaders.sh
@@ -0,0 +1,3 @@
+# !/bin/bash
+glslc assets/shader.frag -o assets/frag.spv
+glslc assets/shader.vert -o assets/vert.spv
--- src/Renderer.hpp
+++ src/Renderer.hpp
@@ -2,34 +2,11 @@
 #ifndef RENDERER_HPP_
 #define RENDERER_HPP_
 
-#include <optional>
-#include <vulkan/vulkan.hpp>
-
-struct QueueFamilyIndices {
-  std::optional<uint32_t> graphicsFamily;
-  std::optional<uint32_t> presentFamily;
-
-  bool IsComplete() { return graphicsFamily.has_value() && presentFamily.has_value(); }
-};
-
-const std::vector<const char*> REQUIRED_DEVICE_EXTENSIONS = {VK_KHR_SWAPCHAIN_EXTENSION_NAME};
-
 class Renderer {
  public:
-  Renderer(vk::PhysicalDevice physicalDevice);
-
-  uint32_t BeginFrame();
-  uint32_t EndFrame();
+  Renderer();
 
  private:
-  void createLogicalDevice(vk::PhysicalDevice physicalDevice);
-  QueueFamilyIndices FindQueueFamilies(vk::PhysicalDevice device);
-
-  vk::Device device;
-  vk::Queue graphicsQueue;
-  vk::Queue presentQueue;
-
-  vk::SwapchainKHR swapchain;
 };
 
 #endif  // RENDERER_HPP_
