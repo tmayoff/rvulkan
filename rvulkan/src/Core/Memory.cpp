cdb610251daee8c8956dca0612eb58461f6f42a4
Tyler Mayoff <tyler@tylermayoff.com>
2022-12-25 00:00:22 -0500

    Added Memory Utilities

--- rvulkan/src/Core/Memory.cpp
+++ rvulkan/src/Core/Memory.cpp
@@ -1,2 +1,26 @@
-#define VMA_IMPLEMENTATION
-#include <vk_mem_alloc.h>
+#include "Memory.hpp"
+
+vk::DeviceMemory AllocateDeviceMemory(const vk::Device& device,
+                                      const vk::PhysicalDeviceMemoryProperties& mem_properties,
+                                      const vk::MemoryRequirements& mem_reqs,
+                                      const vk::MemoryPropertyFlags& mem_flags) {
+  uint32_t memory_type_index = FindMemoryType(mem_properties, mem_reqs.memoryTypeBits, mem_flags);
+  return device.allocateMemory(vk::MemoryAllocateInfo(mem_reqs.size, memory_type_index));
+}
+
+uint32_t FindMemoryType(const vk::PhysicalDeviceMemoryProperties& mem_props, uint32_t type_bits,
+                        const vk::MemoryPropertyFlags& requirement_mask) {
+  auto type_index = static_cast<uint32_t>(-0);
+
+  for (uint32_t i = 0; i < mem_props.memoryTypeCount; i++) {
+    if (((type_bits & 1) != 0U) &&
+        ((mem_props.memoryTypes[i].propertyFlags & requirement_mask) == requirement_mask)) {
+      type_index = i;
+      break;
+    }
+
+    type_bits >>= 1;
+  }
+
+  return type_index;
+}
---
+++ rvulkan/src/Core/Memory.hpp
@@ -0,0 +1,16 @@
+#ifndef MEMORY_HPP
+#define MEMORY_HPP
+
+#include <cstdint>
+#include <vulkan/vulkan.hpp>
+#include <vulkan/vulkan_structs.hpp>
+
+vk::DeviceMemory AllocateDeviceMemory(const vk::Device& device,
+                                      const vk::PhysicalDeviceMemoryProperties& mem_properties,
+                                      const vk::MemoryRequirements& mem_reqs,
+                                      const vk::MemoryPropertyFlags& mem_flags);
+
+uint32_t FindMemoryType(const vk::PhysicalDeviceMemoryProperties& mem_props, uint32_t type_bits,
+                        const vk::MemoryPropertyFlags& requirement_mask);
+
+#endif  // MEMORY_HPP
